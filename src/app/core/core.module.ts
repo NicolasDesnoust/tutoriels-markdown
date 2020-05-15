c096875e5969c6efece1fa02abff192905c638aa
VictoriaNiaba <niaba.victoria@gmail.com>
2020-05-15 17:42:18 +0200

    Layout

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LeftSidenavComponent } from './left-sidenav/left-sidenav.component';
+import { RightSidenavComponent } from './right-sidenav/right-sidenav.component';
+import { MaterialModule } from '../shared/material/material.module';
 
 @NgModule({
-  declarations: [],
+  declarations: [NavbarComponent, LeftSidenavComponent, RightSidenavComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule
+  ],
+  exports: [
+    RightSidenavComponent,
+    NavbarComponent,
+    LeftSidenavComponent
   ]
 })
 export class CoreModule { }
