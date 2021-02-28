5d809119a6ee0cbb4b19c731256254970798bdaf
NicolasDesnoust <desnoust.nicolas451@gmail.com>
2021-02-28 06:42:18 +0100

    fix(pipeline ci): suppression du chemin local de puppeteer

--- scully.tutoriels.config.ts
+++ scully.tutoriels.config.ts
@@ -64,7 +64,7 @@ export const config: ScullyConfig = {
     },
   },
   puppeteerLaunchOptions: {
-    executablePath:
-      'C:\\Users\\desno\\AppData\\Local\\Chromium\\Application\\chrome.exe',
+    // executablePath:
+    //   'C:\\Users\\desno\\AppData\\Local\\Chromium\\Application\\chrome.exe',
   },
 };
