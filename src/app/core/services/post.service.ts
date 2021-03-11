642bc91c823c1950b246da7de66b47ea2e0b7086
NicolasDesnoust <desnoust.nicolas451@gmail.com>
2021-03-11 14:32:02 +0100

    fix(post service): récupération du post courant via un BehaviorSubject

--- src/app/core/services/post.service.ts
+++ src/app/core/services/post.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { ScullyRoutesService } from '@scullyio/ng-lib';
-import { Observable } from 'rxjs';
-import { filter, map } from 'rxjs/operators';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { filter, map, tap } from 'rxjs/operators';
 
 import { Post } from '../model/post';
 import { filterNonBlogRoutes, isABlogRoute } from '../util/blog-utils';
@@ -12,11 +12,29 @@ import { PostAdapterService } from './post-adapter.service';
   providedIn: 'root',
 })
 export class PostService {
+  private currentPostSubject = new BehaviorSubject<Post>(null);
+
   constructor(
     private scully: ScullyRoutesService,
     private postAdapter: PostAdapterService,
     private categoryService: CategoryService
-  ) {}
+  ) {
+    this.initCurrentPostSubject();
+  }
+
+  get currentPost$(): Observable<Post> {
+    return this.currentPostSubject.asObservable();
+  }
+
+  initCurrentPostSubject(): void {
+    this.scully.getCurrent().pipe(
+      tap((scullyRoute) => console.log('current route: ' + JSON.stringify(scullyRoute))),
+      filter((scullyRoute) => isABlogRoute(scullyRoute)),
+      map((rawPost) =>
+        this.postAdapter.toPost(rawPost, this.categoryService.categories)
+      )
+    ).subscribe(this.currentPostSubject);
+  }
 
   get availablePosts$(): Observable<Post[]> {
     return this.scully.available$.pipe(
@@ -24,7 +42,7 @@ export class PostService {
       map((rawPosts) =>
         this.postAdapter.toPosts(rawPosts, this.categoryService.categories)
       )
-    ); 
+    );
   }
 
   get allPosts$(): Observable<Post[]> {
@@ -32,15 +50,6 @@ export class PostService {
       map((scullyRoutes) => filterNonBlogRoutes(scullyRoutes)),
       map((rawPosts) =>
         this.postAdapter.toPosts(rawPosts, this.categoryService.categories)
-      )
-    );
-  }
-
-  get currentPost$(): Observable<Post> {
-    return this.scully.getCurrent().pipe(
-      filter(scullyRoute => isABlogRoute(scullyRoute)),
-      map((rawPost) =>
-        this.postAdapter.toPost(rawPost, this.categoryService.categories)
       )
     );
   }
