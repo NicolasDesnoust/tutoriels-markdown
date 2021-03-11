7dec023df32455646f300b0385498b09f93dccd9
NicolasDesnoust <desnoust.nicolas451@gmail.com>
2021-03-11 17:04:16 +0100

    fix(blog utils): correction de la détection des routes du blog

--- src/app/core/util/blog-utils.ts
+++ src/app/core/util/blog-utils.ts
@@ -4,6 +4,18 @@ export function filterNonBlogRoutes(scullyRoutes: ScullyRoute[]) {
   return scullyRoutes.filter((scullyRoute) => isABlogRoute(scullyRoute));
 }
 
+/**
+ * Vérifie si une route de Scully est celle d'un post du blog.
+ * 
+ * Supporte les routes préfixées (en prod.):
+ * /Desnote-book/blog/...
+ * et les routes non-préfixées (en dev.):
+ * /blog/...
+ * 
+ * @param scullyRoute 
+ * @returns 
+ */
 export function isABlogRoute(scullyRoute: ScullyRoute): boolean {
-  return scullyRoute.route.startsWith('/blog/');
+  const regex: RegExp = new RegExp("\/?[^\/]*\/blog\/[^\/]*", "gm");
+  return regex.test(scullyRoute.route);
 }
