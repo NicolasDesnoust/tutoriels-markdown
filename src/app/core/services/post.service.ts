ef153567f157c07caead00df372d0c4244c2d83b
NicolasDesnoust <desnoust.nicolas451@gmail.com>
2021-03-11 16:11:59 +0100

    debug(post service): ajout de logs pour résoudre le bug du post courant

--- src/app/core/services/post.service.ts
+++ src/app/core/services/post.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { ScullyRoutesService } from '@scullyio/ng-lib';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { filter, map, tap } from 'rxjs/operators';
+import { NavigationEnd, Router } from '@angular/router';
+import { ScullyRoute, ScullyRoutesService } from '@scullyio/ng-lib';
+import { BehaviorSubject, merge, Observable, of } from 'rxjs';
+import { filter, map, switchMap, tap } from 'rxjs/operators';
 
 import { Post } from '../model/post';
 import { filterNonBlogRoutes, isABlogRoute } from '../util/blog-utils';
@@ -17,7 +18,8 @@ export class PostService {
   constructor(
     private scully: ScullyRoutesService,
     private postAdapter: PostAdapterService,
-    private categoryService: CategoryService
+    private categoryService: CategoryService,
+    private router: Router
   ) {
     this.initCurrentPostSubject();
   }
@@ -26,8 +28,50 @@ export class PostService {
     return this.currentPostSubject.asObservable();
   }
 
+  scullyGetCurrent(): Observable<ScullyRoute> {
+    if (!location) {
+      console.log("No location, probably not in a browser");
+      /** probably not in a browser, no current location available */
+      return of();
+    }
+    /** fire off at start, and when navigation is done. */
+    return merge(of(new NavigationEnd(0, '', '')), this.router.events).pipe(
+      tap(e => console.log(JSON.stringify(e.toString()))),
+      filter((e) => e instanceof NavigationEnd),
+      tap(e => console.log(JSON.stringify(e))),
+      switchMap(() => this.scully.available$),
+      map((list: any[]) => {
+        list.forEach(elt => console.log(JSON.stringify("elt: " + JSON.stringify(elt))));
+        console.log(JSON.stringify("location.pathname: " + location.pathname));
+        const curLocation = this.basePathOnly(encodeURI(location.pathname).trim());
+        console.log(JSON.stringify("curLocation: " + curLocation));
+        return list.find(
+          (r) =>
+            curLocation === this.basePathOnly(r.route.trim()) ||
+            (r.slugs &&
+              Array.isArray(r.slugs) &&
+              r.slugs.find((slug) =>
+                curLocation.endsWith(this.basePathOnly(slug.trim()))
+              ))
+        );
+      }),
+      tap(found => console.log("found: " + JSON.stringify(found)))
+    );
+  }
+
+  private basePathOnly = (str: string): string => {
+    if (str.includes('#')) {
+      str = str.split('#')[0];
+    }
+    if (str.includes('?')) {
+      str = str.split('?')[0];
+    }
+    const cleanedUpVersion = str.endsWith('/') ? str.slice(0, -1) : str;
+    return cleanedUpVersion;
+  };
+
   initCurrentPostSubject(): void {
-    this.scully.getCurrent().pipe(
+    this.scullyGetCurrent().pipe(
       tap((scullyRoute) => console.log('current route: ' + JSON.stringify(scullyRoute))),
       filter((scullyRoute) => isABlogRoute(scullyRoute)),
       map((rawPost) =>
